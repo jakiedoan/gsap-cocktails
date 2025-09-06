62f5254d296772b2c1ac42a02ed53e99b25d5d86
Jakie <jakiedoan@gmail.com>
2025-09-07 04:44:15 +0700

    Full ver.

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all';
+
+import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Cocktails from './components/cocktails';
+import About from './components/about';
+import Art from './components/art';
+import Menu from './components/menu';
+import Contact from './components/contact';
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
+  );
+}
+
+export default App;
